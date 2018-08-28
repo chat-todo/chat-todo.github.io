af951aec119ee4652a559871bfd4b4f53c33d5ae
Kapil jain <kjain3474@gmail.com>
2018-08-28 12:36:01 +0530

    Latest

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	$(".carousel").swipe({
+
+		swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+
+			if (direction == 'left') $(this).carousel('next');
+			if (direction == 'right') $(this).carousel('prev');
+
+		},
+		allowPageScroll:"vertical"
+
+	});
+});
