e532f13515c7f2f20d1d9c8e37ea77961d4eeb1b
Sakshi Choudhary <choudharysakshi023@gmail.com>
2021-02-07 12:03:41 +0530

    Merge branch 'master' of https://github.com/chat-todo/chat-todo.github.io

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-$(document).ready(function() {
-
-	$(".carousel").swipe({
-
-		swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
-
-			if (direction == 'left') $(this).carousel('next');
-			if (direction == 'right') $(this).carousel('prev');
-
-		},
-		allowPageScroll:"vertical"
-
-	});
+$(document).ready(function () {
+  $(".carousel").swipe({
+    swipe: function (
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction == "left") $(this).carousel("next");
+      if (direction == "right") $(this).carousel("prev");
+    },
+    allowPageScroll: "vertical",
+  });
 });
